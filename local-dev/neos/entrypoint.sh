3386576efec06fdb2b236cdd22c6a8f179b62082
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2018-11-26 10:26:46 +0100

    Initial commit

---
+++ local-dev/neos/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+composer install
+
+./flow doctrine:migrate
+./flow site:import --package-key Sandstorm.360grad
+./flow user:create --roles Administrator admin password LocalDev Admin
+./flow resource:publish
+
+./flow server:run --host=0.0.0.0
