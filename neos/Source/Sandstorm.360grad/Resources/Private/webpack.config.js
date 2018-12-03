c4073993187caa54e39a37abde7bb302f49cfdd1
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2018-12-03 13:00:20 +0100

    Update neos/Source/Sandstorm.360grad/Resources/Private/webpack.config.js

--- neos/Source/Sandstorm.360grad/Resources/Private/webpack.config.js
+++ neos/Source/Sandstorm.360grad/Resources/Private/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
                         loader: "style-loader" // creates style nodes from JS strings
                     },
                     {
-                        loader: "css-loader" // translates CSS into CommonJS
+                        loader: "css-loader?url=false" // translates CSS into CommonJS
                     },
                     {
                         loader: "sass-loader", // compiles Sass to CSS
