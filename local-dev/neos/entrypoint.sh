ef7f05e24b0ec599c50b5e75bc6847388bd2ff8a
Eric Kloss <eric.kloss@sandstorm.de>
2019-07-16 14:35:36 +0200

    TASK: nginx & php-fpm in one single docker container

--- local-dev/neos/entrypoint.sh
+++ local-dev/neos/entrypoint.sh
@@ -5,8 +5,14 @@ composer install
 
 ./flow doctrine:migrate
 ./flow site:import --package-key $SITE_IMPORT_PACKAGE_KEY
-./flow user:create --roles Administrator $ADMIN_USERNAME $ADMIN_PASSWORD LocalDev Admin
+./flow user:create --roles Administrator $ADMIN_USERNAME $ADMIN_PASSWORD LocalDev Admin || true
 ./flow resource:publish
 
-# run command
-exec "$@"
+# configure nginx
+envsubst '\$NGINX_HOST \$NGINX_PORT' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+
+# start nginx in background
+nginx &
+
+# start PHP-FPM
+exec /usr/local/sbin/php-fpm
