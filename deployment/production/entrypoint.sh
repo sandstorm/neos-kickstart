08a748ae34508a187816080b51c41da51b2de664
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2019-07-23 13:52:17 +0200

    add override for nginx

--- deployment/production/entrypoint.sh
+++ deployment/production/entrypoint.sh
@@ -4,6 +4,9 @@ set -ex
 ./flow doctrine:migrate
 ./flow resource:publish
 
+# configure nginx
+envsubst '\$NGINX_HOST \$NGINX_PORT' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+
 # start nginx
 nginx &
 
