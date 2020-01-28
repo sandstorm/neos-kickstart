2af5d829327f737b9ece4ca1d80a22ed7ef1b397
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2020-01-28 15:22:53 +0100

    Fixed some basic node types

---
+++ app/DistributionPackages/Sandstorm.ProjectX/Classes/Domain/Service/AssetTitleManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Sandstorm\ProjectX\Domain\Service;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Persistence\Exception\IllegalObjectTypeException;
+use Neos\Media\Domain\Model\AssetInterface;
+use Neos\Media\Domain\Repository\AssetRepository;
+
+class AssetTitleManager
+{
+
+    /**
+     * @Flow\Inject
+     * @var AssetRepository
+     */
+    protected $assetRepository;
+
+    /**
+     * @param AssetInterface $asset
+     * @throws IllegalObjectTypeException
+     */
+    public function addTitleToImportedAsset(AssetInterface $asset)
+    {
+        $currentTitle = $asset->getTitle();
+        if(strlen($currentTitle) == 0) {
+            $fileName = $asset->getResource()->getFilename();
+            $fileName = preg_replace('/.[^.]*$/', '', $fileName);
+            $fileName = str_replace(array('-', '_'), ' ', $fileName);
+            $asset->setTitle(ucwords($fileName));
+            $this->assetRepository->update($asset);
+        }
+    }
+}
