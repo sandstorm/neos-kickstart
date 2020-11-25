30fd8f4897811588b5f7cba6087d99bd3f544a27
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2020-11-25 08:47:54 +0100

    Fix broken start up when site was already imported
    Also add a new mount for migrations to make them writable

--- deployment/local-dev/neos/entrypoint.sh
+++ deployment/local-dev/neos/entrypoint.sh
@@ -4,7 +4,13 @@ set -ex
 composer install
 
 ./flow doctrine:migrate
-./flow site:import --package-key $SITE_IMPORT_PACKAGE_KEY
+
+# only run site import when nothing was imported before
+importedSites=`./flow site:list`
+if [ "$importedSites" = "No sites available" ]; then
+    ./flow site:import --package-key $SITE_IMPORT_PACKAGE_KEY
+fi
+
 ./flow user:create --roles Administrator $ADMIN_USERNAME $ADMIN_PASSWORD LocalDev Admin || true
 ./flow resource:publish
 
