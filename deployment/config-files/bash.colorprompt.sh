0895965dbc51c6db141aafde0531e1ee9d8be75e
Florian Seidel <florian.seidel@sandstorm.de>
2021-10-13 15:41:18 +0200

    TASK: Updated dockerfiles for staging and dev

--- deployment/config-files/bash.colorprompt.sh
+++ deployment/config-files/bash.colorprompt.sh
@@ -1,7 +1,7 @@
 # color the prompt according to $SHELL_ENV_DISPLAY variable
 case "$SHELL_ENV_DISPLAY" in
     dev*)
-        PS1='\e[42m$SHELL_ENV_DISPLAY\e[m \u@\h:\w\$ '
+        PS1='\e[42m\n=== $SHELL_ENV_DISPLAY ===\e[m\n\u@\h:\w\$ '
         ;;
     staging*)
         PS1='\e[43m\n=== $SHELL_ENV_DISPLAY ===\e[m\n\u@\h:\w\$ '
