144f0122b38dc473281a4dce04f19c5ebcacc8d0
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2022-05-17 12:50:59 +0200

    Added debug tool for logs and exceptions

---
+++ deployment/utilities/watchAndLogExceptions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+green_echo() {
+  printf "\033[0;32m%s\033[0m\n" "${1}"
+}
+
+red_echo() {
+  printf "\033[0;31m%s\033[0m\n" "${1}"
+}
+
+mkdir -p /app/Data/Logs/Exceptions
+
+inotifywait -m /app/Data/Logs/Exceptions -e create -e moved_to |
+    while read dir action file; do
+        red_echo "################################################################################################"
+        red_echo "### Flow exception: '$file' (directory: '$dir', action: '$action')"
+        # do something with the file
+        cat $dir/$file
+        red_echo "### end exception"
+        red_echo "################################################################################################"
+    done & \
+    tail -f -n 5 /app/Data/Logs/System_Development.log
