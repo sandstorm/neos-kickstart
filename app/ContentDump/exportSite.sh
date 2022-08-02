7a405436a65c4355184240ad8890754aac849b58
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2022-08-02 14:29:30 +0200

    Updated Image NodeType
    * fixed resources export

--- app/ContentDump/exportSite.sh
+++ app/ContentDump/exportSite.sh
@@ -3,7 +3,8 @@
 # IMPORTANT: will run in Container
 echo "Starting Export In Container"
 
-#tar fcz /app/ContentDump/Resources.tar.gz -C /app/Data/Persistent/Resources/ .
+tar fcz /app/ContentDump/Resources.tar.gz -C /app/Data/Persistent/Resources/ .
+
 mysqldump \
     --host=maria-db \
     --user=neos \
