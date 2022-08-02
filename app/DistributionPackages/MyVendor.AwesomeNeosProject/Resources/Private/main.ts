18627abd118de97bfe8b9578856e7a7116483022
Michael Berhorst <michael.berhorst@sandstorm-media.de>
2022-08-02 14:09:34 +0200

    Removed scroll transition example

--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/main.ts
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/main.ts
@@ -2,7 +2,6 @@ import { Application } from "@hotwired/stimulus";
 
 // We decided to use https://stimulus.hotwired.dev/ to write js code
 // as it provides a great way to structure and develop js components.
-import ScrollTransition from "./Fusion/Presentation/Components/ScrollTransition/ScrollTransition";
 
 declare global {
     interface Window {
@@ -14,7 +13,6 @@ declare global {
 const Stimulus = Application.start();
 // we decided to add controllers manually as we want to use our own folder structure
 // for ts/js
-Stimulus.register("ScrollTransition", ScrollTransition);
 Stimulus.handleError = (error, message, detail) => {
     console.warn(message, detail);
     window.ErrorTrackingSystem.captureException(error);
