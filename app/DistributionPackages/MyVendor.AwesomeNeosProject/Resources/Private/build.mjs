9c78c5d673eef57092e423099ee55c8c9c06e3c7
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2022-08-03 19:06:48 +0200

    TASK: add docs how to add tailwind.css (in case one would need this)

--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/build.mjs
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/build.mjs
@@ -12,6 +12,14 @@ esbuild.build({
     sourcemap: true,
     outfile: '../Public/bundle.js',
     external: ['*.woff', '*.woff2'],
+
+    // NOTE: if you want to use Tailwind.css in this setup,
+    // you need to add the following plugin entry, where "postCssPlugin" is imported from "esbuild-plugin-postcss2";
+    // and additionally a tailwind.config.js.
+    //
+    // postCssPlugin.default({
+    //    plugins: [autoprefixer, tailwindcss]
+    // })
     plugins: [sassPlugin()],
 
 
