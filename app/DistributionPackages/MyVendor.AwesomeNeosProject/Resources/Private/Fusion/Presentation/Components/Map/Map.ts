c53aca92d3eba344cb969afbd7db1639432268d4
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2022-09-06 15:53:25 +0200

    TASK: rename lon to lng for consistency with MapLibre

--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/Map/Map.ts
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/Map/Map.ts
@@ -4,21 +4,21 @@ import { Alpine as AlpineType } from 'alpinejs';
 // for the import below to work.
 
 export function initMap(Alpine: AlpineType) {
-    Alpine.data('map', ({lon, lat, zoom, popupText}: any) => ({
+    Alpine.data('map', ({lng, lat, zoom, popupText}: any) => ({
         loadMap() {
             // @ts-ignore
             import('/_maptiles/frontend/v1/map-main.js')
                 .then(({maplibregl, createMap}) => {
                     let map = createMap(window.location.protocol + '//' + window.location.host + '/_maptiles', {
                         container: this.$el,
-                        center: [lon, lat], // starting position [lng, lat]
+                        center: [lng, lat], // starting position [lng, lat]
                         zoom: zoom, // starting zoom
                     });
 
                     map.addControl(new maplibregl.NavigationControl(), 'top-left');
 
                     new maplibregl.Marker()
-                        .setLngLat([lon, lat])
+                        .setLngLat([lng, lat])
                         .setPopup(new maplibregl.Popup({ offset: 25 }).setText(popupText))
                         .addTo(map);
                 });
