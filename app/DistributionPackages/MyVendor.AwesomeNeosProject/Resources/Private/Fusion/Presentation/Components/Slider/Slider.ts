fb297bb7983878108d8ab1e6fe1ab950a155ec92
Anne Menzel <anne.menzel@sandstorm.de>
2023-02-16 16:56:04 +0100

    TASK: replace glidejs with swiperjs

--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/Slider/Slider.ts
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/Slider/Slider.ts
@@ -1,26 +1,25 @@
 // @ts-ignore
-import Glide from '@glidejs/glide'
+import Swiper from 'swiper/bundle';
+// import Swiper styles
+import 'swiper/css/bundle';
 
 export default (inBackend: unknown = false) => ({
     inBackend: inBackend as boolean,
     _currentPosition: 1,
 
-    // init is called before alpine.js renders the appropriate component in the DOM.
-    // In this case we create a new Glide Slider for the referenced Slider (x-ref="glide") in the DOM.
-    // Have a look at https://alpinejs.dev/component/glide on how to use and extend the slider/glide
     init() {
         const scope = this;
         // listen to node created/selected events to scroll to the right slide in neos backend
         document.addEventListener('Neos.NodeCreated', function(event) {
             // @ts-ignore
             const selectedElement = event.detail.element as HTMLElement;
-            scope._scrollSlideIntoView(selectedElement.closest(".glide__slide"));
+            scope._scrollSlideIntoView(selectedElement.closest(".swiper-slide"));
         }, false);
 
         document.addEventListener('Neos.NodeSelected', function(event) {
             // @ts-ignore
             const selectedElement = event.detail.element as HTMLElement;
-            scope._scrollSlideIntoView(selectedElement.closest(".glide__slide"));
+            scope._scrollSlideIntoView(selectedElement.closest(".swiper-slide"));
         }, false);
 
 
@@ -46,7 +45,7 @@ export default (inBackend: unknown = false) => ({
         }
 
         // @ts-ignore
-        const amountOfSlides = this.$refs.glide.querySelectorAll('.glide__slide').length;
+        const amountOfSlides = this.$refs.slider.querySelectorAll('.swiper-slide').length;
         if (amountOfSlides === 0) {
             return;
         }
@@ -57,33 +56,44 @@ export default (inBackend: unknown = false) => ({
 
         this._currentPosition = newPosition
         // @ts-ignore
-        const slider = this.$refs.glide;
-        const slideToScrollTo = slider.querySelector(".glide__slide:nth-child(" + newPosition + ")");
+        const slider = this.$refs.slider;
+        const slideToScrollTo = slider.querySelector(".swiper-slide:nth-child(" + newPosition + ")");
         this._scrollSlideIntoView(slideToScrollTo);
     },
 
     _scrollSlideIntoView(slide: HTMLElement|null) {
         if (slide) {
             // @ts-ignore
-            const slider = this.$refs.glide;
-            slider.querySelector(".glide__slides").scrollLeft = slide.offsetLeft;
+            const slider = this.$refs.slider;
+            slider.querySelector(".swiper-wrapper").scrollLeft = slide.offsetLeft;
         }
     },
 
     _initSlider() {
-        // The slider is not initialized with glide when in the neos-backend to prevent broken rendering
+        // The slider is not initialized with swiper when in the neos-backend to prevent broken rendering
         if (this.inBackend) {
             return;
         }
+
         // @ts-ignore
-        const amountOfSlides = this.$refs.glide.querySelectorAll('.glide__slide').length;
+        const swiperRef = this.$refs.slider;
+
+        // @ts-ignore
+        const amountOfSlides = swiperRef.querySelectorAll('.swiper-slide').length;
         if (amountOfSlides === 0) {
             return;
         }
-        // @ts-ignore
-        new Glide(this.$refs.glide, {
-            type: 'carousel',
-            perView: 1
-        }).mount()
+
+        new Swiper(swiperRef, {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
     }
 })
