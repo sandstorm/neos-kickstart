0d08d65dc20916a26f6b02629811249527eaed00
Anne Menzel <anne.menzel@sandstorm.de>
2023-02-20 16:18:03 +0100

    TASK: add different backend handling with initialized slider

--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/Slider/Slider.ts
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/Slider/Slider.ts
@@ -2,79 +2,78 @@
 import Swiper from 'swiper/bundle';
 // import Swiper styles
 import 'swiper/css/bundle';
+import {Swiper as SwiperType} from "swiper";
+
+const SLIDE_CLASS = "slider__slide";
+const SLIDER_CLASS = "slider";
 
 export default (inBackend: unknown = false) => ({
     inBackend: inBackend as boolean,
     _currentPosition: 1,
+    swiper: null as SwiperType | null,
 
+    // init is called before alpine.js renders the appropriate component in the DOM.
+    // In this case we create a new Swiper for the referenced Slider (x-ref="slider") in the DOM.
     init() {
         const scope = this;
-        // listen to node created/selected events to scroll to the right slide in neos backend
-        document.addEventListener('Neos.NodeCreated', function(event) {
-            // @ts-ignore
-            const selectedElement = event.detail.element as HTMLElement;
-            scope._scrollSlideIntoView(selectedElement.closest(".swiper-slide"));
-        }, false);
-
-        document.addEventListener('Neos.NodeSelected', function(event) {
-            // @ts-ignore
-            const selectedElement = event.detail.element as HTMLElement;
-            scope._scrollSlideIntoView(selectedElement.closest(".swiper-slide"));
-        }, false);
-
 
         this._initSlider();
-    },
-
-    // used only in backend
-    slideLeft(event: PointerEvent) {
-        const newPosition = this._currentPosition - 1;
-        this._slideTo(newPosition);
-    },
-
-    // used only in backend
-    slideRight(event: PointerEvent) {
-        const newPosition = this._currentPosition + 1;
-        this._slideTo(newPosition);
-    },
-
-    // Custom slide handling for the neos backend
-    _slideTo(newPosition: number) {
-        if (!this.inBackend) {
-            return;
+        if (this.inBackend) {
+            // listen to node events to scroll to the right slide in neos backend
+            document.addEventListener('Neos.NodeCreated', function (event) {
+                scope.nodeCreated(event)
+            }, false);
+            document.addEventListener('Neos.NodeSelected', function (event) {
+                scope.nodeSelected(event)
+            }, false);
+            document.addEventListener('Neos.NodeRemoved', function (event) {
+                scope.nodeRemoved(event)
+            }, false);
         }
+    },
 
+    // Backend Optimizations
+    getSlideIndex(element: HTMLElement): number {
         // @ts-ignore
-        const amountOfSlides = this.$refs.slider.querySelectorAll('.swiper-slide').length;
-        if (amountOfSlides === 0) {
-            return;
-        }
-
-        if (newPosition < 1 || newPosition > amountOfSlides) {
-            return;
-        }
-
-        this._currentPosition = newPosition
-        // @ts-ignore
-        const slider = this.$refs.slider;
-        const slideToScrollTo = slider.querySelector(".swiper-slide:nth-child(" + newPosition + ")");
-        this._scrollSlideIntoView(slideToScrollTo);
+        const slides = Array.from(this.$refs.slider.querySelectorAll(`.${SLIDE_CLASS}`));
+        return slides.indexOf(element);
     },
 
-    _scrollSlideIntoView(slide: HTMLElement|null) {
-        if (slide) {
-            // @ts-ignore
-            const slider = this.$refs.slider;
-            slider.querySelector(".swiper-wrapper").scrollLeft = slide.offsetLeft;
+    isSlide(element: HTMLElement): boolean {
+        return element.classList.contains(SLIDE_CLASS);
+    },
+
+    isOwnSlide(element: HTMLElement): boolean {
+        if (!this.isSlide(element)) return false;
+        // @ts-ignore
+        return element.closest(`.${SLIDER_CLASS}`) === this.$refs.slider;
+    },
+
+    nodeSelected(event: any) {
+        if (this.swiper && this.isOwnSlide(event.detail.element)) {
+            const index = this.getSlideIndex(event.detail.element);
+            if (index >= 0) {
+                this.swiper.update();
+                this.swiper.slideTo(index);
+            }
+        }
+    },
+
+    nodeRemoved(event: any) {
+        if (this.swiper && this.isSlide(event.detail.element)) {
+            // we update all sliders in the page as the parent is null
+            // and we cannot check if it is our own slide
+            this.swiper.update();
+        }
+    },
+
+    nodeCreated(event: any) {
+        if (this.swiper && this.isOwnSlide(event.detail.element)) {
+            this.swiper.update();
         }
     },
 
     _initSlider() {
-        // The slider is not initialized with swiper when in the neos-backend to prevent broken rendering
-        if (this.inBackend) {
-            return;
-        }
-
         // @ts-ignore
         const swiperRef = this.$refs.slider;
 
@@ -84,7 +83,7 @@ export default (inBackend: unknown = false) => ({
             return;
         }
 
-        new Swiper(swiperRef, {
+        const swiperOptions = {
             loop: true,
             pagination: {
                 el: '.swiper-pagination',
@@ -94,6 +93,12 @@ export default (inBackend: unknown = false) => ({
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             },
-        });
+        }
+
+        // this would otherwise create copies of the slides breaking
+        // the index calculation
+        if (this.inBackend) swiperOptions.loop = false;
+
+        this.swiper = new Swiper(swiperRef, swiperOptions);
     }
 })
